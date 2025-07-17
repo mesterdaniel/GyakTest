887fab73af9542c1536173cd1c7dae1a90fb6504
mesterdaniel <112866542+mesterdaniel@users.noreply.github.com>
2025-07-17 10:50:20 +0200

    Updated WebhookController

--- src/main/java/com/BC/SzakGyak/controller/TwilioWebhookController.java
+++ src/main/java/com/BC/SzakGyak/controller/TwilioWebhookController.java
@@ -1,18 +1,22 @@
 package com.BC.SzakGyak.controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
 @RestController
 public class TwilioWebhookController {
 
-    @PostMapping("/twilio/webhook")
-    public ResponseEntity<String> receiveMessage(@RequestBody String payload) {
-        // Logolják a payload adatokat, hogy lásd mi érkezik be
-        System.out.println("Received payload: " + payload);
+    @PostMapping("/webhook/twilio")
+    public ResponseEntity<String> receiveMessage(@RequestParam("From") String from,
+                                                 @RequestParam("Body") String body) {
 
-        // Ha minden rendben, 200 OK válasz küldése
-        return ResponseEntity.ok("Received");
+        System.out.println("Üzenet érkezett: " + body + " - Feladótól: " + from);
+        String responseMessage = "<Response><Message>Üzeneted megkaptuk!</Message></Response>";
+
+
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(responseMessage);
     }
 }
