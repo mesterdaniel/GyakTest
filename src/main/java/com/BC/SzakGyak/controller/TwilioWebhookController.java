6f585711a9165f075d7789962996b11b46ccac45
mesterdaniel <112866542+mesterdaniel@users.noreply.github.com>
2025-07-17 14:10:28 +0200

    Update TwilioWebhookController.java

--- src/main/java/com/BC/SzakGyak/controller/TwilioWebhookController.java
+++ src/main/java/com/BC/SzakGyak/controller/TwilioWebhookController.java
@@ -1,4 +1,6 @@
 package com.BC.SzakGyak.controller;
+import com.BC.SzakGyak.service.TwilioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,13 +10,18 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 public class TwilioWebhookController {
-
+    @Autowired
+    private TwilioService twilioService;
     @PostMapping("/webhook/twilio")
     public ResponseEntity<String> receiveMessage(@RequestParam("From") String from,
                                                  @RequestParam("Body") String body) {
 
-        System.out.println("Üzenet érkezett: " + body + " - Feladótól: " + from);
+        //System.out.println("Üzenet érkezett: " + body + " - Feladótól: " + from);
         String responseMessage = "<Response><Message>Üzeneted megkaptuk!</Message></Response>";
+        if (body.toLowerCase() == "menu") {
+            responseMessage= twilioService.menu();
+        }
+
 
 
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(responseMessage);
